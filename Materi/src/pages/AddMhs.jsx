a6111b4b92f2e24cae3603ba5230f3b84f07ec16
Dionisius Mikha <64204877+DionisiusMikha@users.noreply.github.com>
2023-11-10 17:46:48 +0700

    upload materi minggu 5
    agak ngeng ya ges ya

---
+++ Materi/src/pages/AddMhs.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { addMhs } from '../app/mhsSlice';
+import { useDispatch } from 'react-redux';
+
+const AddMhs = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const dispatch = useDispatch()
+  const actionAddMhs = data =>{
+    try{
+      dispatch(addMhs(data))
+      reset()
+      alert("Berhasil menambahkan data mahasiswa baru")
+    }catch(e){
+      alert(e.message)
+    }
+  }
+  return (
+    <>
+    
+      <h1>Profile</h1>
+      <div>
+      <h1> gold : </h1>
+      </div>
+      <p>day : 1</p> <button>Next Day</button>
+      {/* <form onSubmit={handleSubmit(actionAddMhs)}>
+        <label>NRP </label> <input type="text" {...register("nrp")} /> <br />
+        <label>Nama </label> <input type="text"  {...register("nama")} /><br />
+        <label>Jumlah SKS </label> <input type="number"  {...register("jumlah_sks")} /> <br />
+        <label>Gender</label> 
+        <input type="radio" name="gender" value={"L"}  {...register("gender")}/>Laki-Laki 
+        <input type="radio" name="gender" value={"P"}  {...register("gender")}/>Perempuan <br />
+
+        <button type="submit">Add</button>
+      </form> */}
+    </>
+  )
+}
+
+export default AddMhs
